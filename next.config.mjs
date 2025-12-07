1d1b58ea39d72ff9509e18e9c9d291f4bf7cd8ac
O-Pan-Weaver <ofentse@truweaver.com>
2025-12-07 18:19:45 +0000

    Fix DATABASE_URL env and upgrade Next build working

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // If you had other options (images, redirects, etc.), put them here.
+  // Example:
+  // reactStrictMode: true,
+};
+
+export default nextConfig;
